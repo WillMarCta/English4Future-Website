c7e18a17391d7eff18070a07922655bff621ea5c
Wilson Marquina <wmjosemarquez@gmail.com>
2026-02-19 19:47:43 -0400

    adding auth

--- english4future/urls.py
+++ english4future/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages import views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.English4Future)
+    path('', views.English4Future),
+    path('signup/', user_views.user_signup, name='signup')
 ]
