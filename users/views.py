963a38bf8109b971f5173b644a21f322ac60e633
Wilson Marquina <wmjosemarquez@gmail.com>
2026-02-20 20:54:37 -0400

    authentication in progress

--- users/views.py
+++ users/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
 def user_signup(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm()})
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
+        })
+    
+    else:
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signup.html', {'form': form})
