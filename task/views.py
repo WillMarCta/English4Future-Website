70f5c64b1748fe945276b54ab8ed70f71c5b66fb
Wilson Marquina <wmjosemarquez@gmail.com>
2026-05-24 18:45:25 -0400

    fix: add missing and security decorator to task views

--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from users.decorators import is_student
 from .models import Task
@@ -11,7 +11,7 @@ def task_list(request):
     tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks.html', {"tasks": tasks})
 
-
+@login_required 
 def complete_by_student(request, task_id):
     task = get_object_or_404(Task, id=task_id, user=request.user)
     task.completed_by_student = True
